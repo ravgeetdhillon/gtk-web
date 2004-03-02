8087d33c1286f0c466b41f0bd16e3f106958f8d4
Owen Taylor <otaylor@src.gnome.org>
2004-03-02 03:40:29 +0000

    add plan/meetings to install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ make_link () {
   ( cd $INSTALLDIR/$todir && rm -f $tobase && ln -s ../$from $tobase )
 }
 
-for i in api apps beos download faq news screenshots success plan plan/2.4 plan/2.6 ; do
+for i in api apps beos download faq news screenshots success plan plan/2.4 plan/2.6 plan/meetings ; do
   for j in site_top.html site_bottom.html box_middle.html box_end.html box_begin.html section_end.html section_begin.html ; do
     make_link $j $i/$j
   done
