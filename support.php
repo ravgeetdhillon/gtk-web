8f7e6e0eeca7ec6a3c5e5202b210d7144af35e9c
Martyn Russell <martyn@lanedo.com>
2012-03-19 15:01:23 +0000

    support: Randomise businesses listed and improve general content

--- support.php
+++ support.php
@@ -15,25 +15,24 @@ ob_start();
 </div>
 
 <h2><a name="GetSupportFor">Get Assistance</a></h2>
-<p>GTK+ is supported by a number of businesses and they provide services, experience and skills to help the large community of GTK+ developers produce innovative customer solutions in multiple industries and devices (like mobile and embedded).</p>
+<p>GTK+ is supported by a number of businesses and they provide services, experience and skills to help the large community of GTK+ users produce innovative customer solutions in multiple industries and devices (like mobile and embedded).</p>
 
-<p>There are many businesses which are help with:</p>
+<p>Consultancy partners which support GTK+ offer a range of business services including:</p>
 <ul>
 	<li><a href="#Consultation">Consultation</a></li>
-	<li><a href="#ApplicationDevelopment">Application development</a></li>
 	<li><a href="#HardwareIntegration">Hardware integration</a></li>
 	<li><a href="#Porting">Porting</a></li>
-	<li><a href="#Training">Training</a></li>
+	<li><a href="#ApplicationDevelopment">Application development</a></li>
 	<li><a href="#BackFolding">Patch set back folding</a></li>
+	<li><a href="#Training">Training</a></li>
 </ul>
 
-<h3>Businesses</h3>
+<h3>Consultancy Partners</h3>
 
 <?php
-// How about we have PHP automatically generate odd rows for us?
 $businesses = array(
-        /* ("Company",                   "Website"), */
-	array("Lanedo GmbH",             "http://lanedo.com/gtk.html"),
+	/* ("Company",                   "Website"), */
+	array("Lanedo GmbH",             "http://www.lanedo.com/gtk+"),
 	array("Openismus GmbH",          "http://www.openismus.com/about"),
 	array("Codethink Ltd",           "http://www.codethink.co.uk/contact"),
 	array("Collabora Ltd",           "http://www.collabora.com/contact/"),
@@ -41,35 +40,60 @@ $businesses = array(
 	array("Novell",                  "http://www.novell.com/support"),
 	array("Intel",                   "http://www.intel.com/p/en_US/support")
 );
+
+function array_random_assoc($arr, $num = 1) {
+    $keys = array_keys($arr);
+    shuffle($keys);
+
+    $r = array();
+    for ($i = 0; $i < $num; $i++) {
+        $r[$keys[$i]] = $arr[$keys[$i]];
+    }
+
+    return $r;
+}
+
+$r = array_random_assoc($businesses, sizeof($businesses));
+/* print_r($r) */
 ?>
 
 <table>
 	<tr>
 		<th>Companies</th>
 		<th>Websites</th>
-	</tr><?php foreach($businesses as $index => $business) { ?> 
-	<tr<?php echo $index % 2 != 0 ? ' class="odd"': ' class="even"'; ?>>
-		<td><?php echo $business[0]; ?></td>
-		<td><a href="<?php echo $business[1]; ?>"><?php echo $business[1]; ?></a></td>
-	</tr><?php } ?> 
+	</tr>
+
+  <?php
+	  $i = 0;
+    foreach($r as $index => $b) {
+	?>
+
+	<tr<?php echo $i % 2 != 0 ? ' class="odd"': ' class="even"'; ?>>
+    <td><?php echo $b[0]; ?></td>
+    <td><a href="<?php echo $b[1]; ?>"><?php echo $b[1]; ?></a></td>
+	</tr>
+
+  <?php $i++;	} ?>
+
+  <tr><td colspan="2"><div id="note">Not listed above? Want to be? Contact the gtk-devel <a href="/mailing-lists.php">mailing list</a>.</div></td></tr>
 </table>
 
 <h3><a name="Consultation">Consultation</a></h3>
-<p>Perhaps your business is looking for consultancy around using GTK+. If this is what you're looking for, companies affilliated with GTK+ can offer these services.</p>
-
-<h3><a name="ApplicationDevelopment">Application Development</a></h3>
-<p>Should you need application development for your solution to be deliverable cross platform, GTK+ has all the tools you need to do that. Contact us today and find out how we can help you.</p>
+<p>Perhaps your business is looking for consultancy around using GTK+. If this is what you&apos;re looking for, our consultancy partners affilliated with GTK+ can offer these services. This can range from development, to support, to even queries about licensing and how to integrate the toolkit into your existing solutions and much more!</p>
 
 <h3><a name="HardwareIntegration">Hardware Integration</a></h3>
-<p>With changes happening all the time in hardware, GTK+ is constantly adapting to the requests for newer and richer user interface features. One great example of those is Multi-Touch support. Businesses invested in GTK+ have been at the forefront of implementing this hardware support and working with other projects up and down the Linux stack to make that happen (such as Xorg). If you need hardware integration with GTK+, contact us to find out more.</p>
+<p>With changes happening all the time in hardware, GTK+ is constantly adapting to the requests for newer and richer user interface features. One great example of those is Multi-Touch support. Businesses invested in GTK+ have been at the forefront of implementing this hardware support and working with other projects up and down the Linux stack to make that happen (such as Xorg).</p>
 
 <h3><a name="Porting">Porting</a></h3>
 <p>Porting can cover a range of areas. Businesses supporting GTK+ offer porting applications to GTK+ from other toolkits and also porting areas of the toolkit to specific platforms (for example, Windows 64bit support).</p>
 
-<h3><a name="Training">Training</a></h3>
-<p>There are various training opportunities on offer. Everything from writing an application with GTK+ to developing and customizing GTK+. If this is what you're looking for, there are businesses out there that can help you do that.</p>
+<h3><a name="ApplicationDevelopment">Application Development</a></h3>
+<p>Should you need application development for your solution to be deliverable cross platform, GTK+ has all the tools you need to do that. Most of the businesses who are involved with GTK+ have decades of experience on offer to assist you.</p>
 
 <h3><a name="BackFolding">Patch Set Back-Folding</a></h3>
-<p>It's not uncommon for companies to start forking a project to customize it and perhaps to keep it in-house. This can lead to wanting new features from an upstream project like GTK+ being available in your forked version. If you need help back-folding patch sets into upstream GTK+ or suggesting new features into the main stream, we have a support network that can help you do that.</p>
+<p>It&apos;s not uncommon for companies to start forking a project to customize it and perhaps to keep it in-house. This can lead to wanting new features from an upstream project like GTK+ being available in your forked version. If you need help back-folding patch sets into upstream GTK+ or suggesting new features into the main stream, we have a support network that can help you do that.</p>
+
+<h3><a name="Training">Training</a></h3>
+<p>There are various training opportunities on offer. Everything from writing an application with GTK+ to developing and customizing GTK+. If this is what you&apos;re looking for, there are businesses out there that can help you do that.</p>
 
 <?php require 'template.php';
