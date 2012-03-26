1860fc66ca662cf25d4dac7b8f9687201e44e134
Javier Jardón <jjardon@gnome.org>
2012-03-26 16:12:17 +0100

    download/linux.php: Current GTK+ version is 3.4

--- download/linux.php
+++ download/linux.php
@@ -5,7 +5,7 @@ ob_start();
 
 // Set the current stable version numbers of GTK+, GLib, and Pango to automatically include it in the page.
 $versions = array(
-	'gtk' => '3.2',
+	'gtk' => '3.4',
 	'glib' => '2.32',
 	'pango' => '1.28',
 	'gdk-pixbuf' => '2.26',
@@ -13,7 +13,7 @@ $versions = array(
 );
 
 // Versions of GTK+ that are older, but still included on the page in the "Older Versions" section
-$oldgtk = array( '3.0', '2.24', '2.20', '2.18', '2.16', '2.14', '2.12', '2.10', '2.8', '2.6', '2.4', '1.2' );
+$oldgtk = array( '3.2', '3.0', '2.24', '2.20', '2.18', '2.16', '2.14', '2.12', '2.10', '2.8', '2.6', '2.4', '1.2' );
 
 ?>
 <h2><a name="Download">Download for GNU/Linux and Unix</a></h2>
