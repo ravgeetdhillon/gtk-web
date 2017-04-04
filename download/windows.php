676dffc12cfa106607de4172836906913bdeb7e5
Luis Menina <liberforce@freeside.fr>
2017-04-04 13:18:17 +0200

    download/windows: Add external class to ext. links
    https://bugzilla.gnome.org/show_bug.cgi?id=779994

--- download/windows.php
+++ download/windows.php
@@ -16,12 +16,12 @@ ob_start();
 
 <h3>Installing GTK+ and its dependencies</h3>
 
-<p>The easiest way to install GTK+ and its dependencies in binary form is through the <a href="https://msys2.github.io/">MSYS2</a> project. MSYS2 provides a UNIX-like development environment for Windows. It provides packages for many software applications and libraries, including the GTK+ stack. These packages are installed using the <a href="https://github.com/msys2/msys2/wiki/MSYS2-installation#iv-general-package-management">pacman package manager</a>.</p>
+<p>The easiest way to install GTK+ and its dependencies in binary form is through the <a class="external" href="https://msys2.github.io/">MSYS2</a> project. MSYS2 provides a UNIX-like development environment for Windows. It provides packages for many software applications and libraries, including the GTK+ stack. These packages are installed using the <a class="external" href="https://github.com/msys2/msys2/wiki/MSYS2-installation#iv-general-package-management">pacman package manager</a>.</p>
 
 <p>Note: in the following steps, we'll assume you're using a 64-bits Windows. Therefore, the package names include the <tt>x86_64</tt> architecture identifier. If you're using a 32-bits Windows, please adapt the instructions below using the <tt>i686</tt> architecture identifier.</p>
 
 <h4>Step 1: Install MSYS2</h4>
-<a href="https://msys2.github.io/">Download the MSYS2 installer</a> that matches your platform and follow the installation instructions.
+<a class="external" href="https://msys2.github.io/">Download the MSYS2 installer</a> that matches your platform and follow the installation instructions.
 
 <h4>Step 2: Install GTK+3 and its dependencies</h4>
 
@@ -54,13 +54,13 @@ You can then import your GUI from your code using the <tt>GtkBuilder</tt> API. R
 <h2>Building and distributing your application</h2>
 
 <h3>Building your application with MSYS2</h3>
-<p>You may use MSYS2 to <a href="https://blogs.gnome.org/nacho/2014/08/01/how-to-build-your-gtk-application-on-windows/">build your GTK+ application and create an installer to distribute it</a>. Your installer will need to ship your application build artifacts as well as GTK+ binaries and runtime dependencies.</p>
+<p>You may use MSYS2 to <a class="external" href="https://blogs.gnome.org/nacho/2014/08/01/how-to-build-your-gtk-application-on-windows/">build your GTK+ application and create an installer to distribute it</a>. Your installer will need to ship your application build artifacts as well as GTK+ binaries and runtime dependencies.</p>
 
 <h3>Building your application with Visual Studio</h3>
-<p>GTK+ releases provide project files for Microsoft Visual Studio. You can use them to build GTK+ itself and use it in your own project. Learn more on <a href="https://wiki.gnome.org/Projects/GTK+/Win32/MSVCCompilationOfGTKStack">how to build the GTK+ stack using Microsoft Visual Studio</a> and read these other tips on <a href="https://blogs.gnome.org/nacho/2015/02/19/building-gtk-3-with-msvc-2013/">how to build GTK+ with MSVC on Windows</a>.</p>
+<p>GTK+ releases provide project files for Microsoft Visual Studio. You can use them to build GTK+ itself and use it in your own project. Learn more on <a class="external" href="https://wiki.gnome.org/Projects/GTK+/Win32/MSVCCompilationOfGTKStack">how to build the GTK+ stack using Microsoft Visual Studio</a> and read these other tips on <a class="external" href="https://blogs.gnome.org/nacho/2015/02/19/building-gtk-3-with-msvc-2013/">how to build GTK+ with MSVC on Windows</a>.</p>
 
 <h3>Distributing GTK+ with your application</h3>
-<p>You are welcome to redistribute GTK+ binaries, including applications that bundle them, on other web sites, CD-ROM, and other media. You don't have to ask for permission. That's one of the points of Free Software. One important thing that the <a href="http://www.fsf.org/licenses/licenses.html">GNU licenses</a> require is that <strong>you must also redistribute the source code</strong>. This usually means at least the gettext, GLib, GTK+, Pango and Atk sources.</p>
+<p>You are welcome to redistribute GTK+ binaries, including applications that bundle them, on other web sites, CD-ROM, and other media. You don't have to ask for permission. That's one of the points of Free Software. One important thing that the <a class="external" href="http://www.fsf.org/licenses/licenses.html">GNU licenses</a> require is that <strong>you must also redistribute the source code</strong>. This usually means at least the gettext, GLib, GTK+, Pango and Atk sources.</p>
 
 <h4>List of GTK+ dependencies</h4>
 <p>GTK+ depends on several libraries:
