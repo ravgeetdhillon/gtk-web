72839478b767d9033e4234f13ef8d5ec0c7950a7
ravgeetdhillon <ravgeetdhillon@gmail.com>
2019-08-08 18:46:19 +0530

    added moment package through npm

--- structurize.sh
+++ structurize.sh
@@ -3,5 +3,6 @@ mv node_modules/jquery assets/ -f
 mv node_modules/popper.js assets/ -f
 mv node_modules/@fortawesome assets/ -f
 mv node_modules/slick-carousel assets/ -f
+mv node_modules/moment assets/ -f
 rm -rf node_modules
 sass assets/scss/theme.scss assets/css/theme.css
