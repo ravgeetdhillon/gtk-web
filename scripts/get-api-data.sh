44b8f3b1868041a782242109593f6f1f50b016c1
ravgeetdhillon <ravgeetdhillon@gmail.com>
2019-08-13 21:27:30 +0530

    updated gitlab ci

--- scripts/get-api-data.sh
+++ scripts/get-api-data.sh
@@ -19,5 +19,3 @@ echo 'successfully fetched gtk-issue-statistics'
 curl https://gitlab.gnome.org/api/v4/projects/665/issues?state=opened > ../_data/api_fetch/tags.json
 
 echo 'successfully fetched gtk-issues'
-
-$SHELL
