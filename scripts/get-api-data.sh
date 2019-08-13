7dce4f6e07f6f3e5b84213b3a12d60d65e3974b5
ravgeetdhillon <ravgeetdhillon@gmail.com>
2019-08-13 21:50:04 +0530

    updated gitlab ci

--- scripts/get-api-data.sh
+++ scripts/get-api-data.sh
@@ -1,6 +1,6 @@
 echo 'creating directory for storing api results...'
 
-mkdir ../api_fetch
+mkdir ../_data/api_fetch
 
 echo 'getting the api data from the gitlab...'
 
@@ -19,3 +19,5 @@ echo 'successfully fetched gtk-issue-statistics'
 curl https://gitlab.gnome.org/api/v4/projects/665/issues?state=opened > ../_data/api_fetch/tags.json
 
 echo 'successfully fetched gtk-issues'
+
+ls ../_data/api_fetch
--- scripts/structurize.sh
+++ scripts/structurize.sh
@@ -1,5 +1,7 @@
 echo 'structurizing the website...'
 
+ls ../node_modules
+
 mv ../node_modules/* ../assets/ -f
 
 rm -rf ../node_modules
