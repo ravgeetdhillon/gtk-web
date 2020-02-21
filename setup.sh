79c1b07b81d5323c545444c8b7f216d2755083e3
ravgeetdhillon <ravgeetdhillon@gmail.com>
2020-02-21 21:31:16 +0530

    install bundle version as specified by gemfile.lock

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+# install npm dependencies
+npm install
+
+
+# install gem dependencies
+gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
+bundle install
+
+
 # script for fetching api data
 echo 'creating directory for storing api results...'
 mkdir _data/api_fetch
