de26e5bb3385ad2f099d66f9d0cbe1d090479398
Emmanuele Bassi <ebassi@gnome.org>
2020-12-16 13:07:09 +0000

    Point to the latest 4.0 stable release
    Now that we have it.
    
    Drop the "latest devel", since we cannot get the tags using a regular
    expression.

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ bundle install
 echo 'creating directory for storing api results...'
 mkdir _data/api_fetch
 echo 'getting the api data from the gitlab...'
-curl -o- https://gitlab.gnome.org/api/v4/projects/665/repository/tags?search=^3.24 > _data/api_fetch/stable_tags.json
-curl -o- https://gitlab.gnome.org/api/v4/projects/665/repository/tags?search=^3.9 > _data/api_fetch/devel_tags.json
+curl -o- https://gitlab.gnome.org/api/v4/projects/665/repository/tags?search=^3.24 > _data/api_fetch/old_stable_tags.json
+curl -o- https://gitlab.gnome.org/api/v4/projects/665/repository/tags?search=^4.0 > _data/api_fetch/stable_tags.json
 echo 'successfully fetched gtk-release-tags'
 curl -o- https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fblog.gtk.org%2Ffeed%2F > _data/api_fetch/gtk-blog-feed.json
 echo 'successfully fetched the gtk blog feed in json format'
